12ba9f99d27f75c384989dcaf3844fb793a94071
adedotxn <phyf3script@gmail.com>
2022-09-15 10:40:18 -0700

    diigit validity

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit checks if c is a digit
+ * @c : character to check for valididity
+ * Return : 1 or 0
+ */
+
+int _isdigit(int c) {
+	return (c >= 48 && c <= 57);
+}
