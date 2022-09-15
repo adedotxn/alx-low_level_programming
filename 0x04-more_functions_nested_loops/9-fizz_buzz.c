e75ab71279b372f468bb7387ea895fbe1b95a05d
adedotxn <phyf3script@gmail.com>
2022-09-15 14:13:21 -0700

    9 to 10

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - Write a program that prints the numbers from 1 to 100, followed by a new line. 
- * But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz.
- * For numbers which are multiples of both three and five print FizzBuzz
+ * main - Program prints numbers from 1 to 100, followed by a new line.
+ * But for multiples of 3 print Fizz instead of the number and
+ * for the multiples of five print Buzz.
+ * For numbers which are multiples of 3 & 5 print FizzBuzz
  * Return: Always 0
  */
 int main(void)
