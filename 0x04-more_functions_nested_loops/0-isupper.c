ed9691b3f0182d900b3ebbbe7fef12804a00d10e
adedotxn <phyf3script@gmail.com>
2022-09-15 10:10:49 -0700

    uppercase check

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/*
+ * Returns 1 if c is uppercase
+ * Returns 0 otherwise
+ * isupper - A function that checks for uppercase character.
+ */
+int _isupper(int c){
+	return (c >= 65 && c <= 90);
+}
