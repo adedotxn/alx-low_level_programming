d92739676f3f46250fe4f9d1e7fb964898a57d32
adedotxn <phyf3script@gmail.com>
2022-09-15 10:32:17 -0700

    should work

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,23 @@
 #include "main.h"
 
-/*
- * Returns 1 if c is uppercase
- * Returns 0 otherwise
- * isupper - A function that checks for uppercase character.
+/**
+ * _isupper - A function that checks for uppercase character.
+ * @c: An input character
+ * Return: 1 if c is uppercase or 0 otherwise
  */
 int _isupper(int c)
 {
-	return (c >= 65 && c <= 90);
+	char uppercase = 'A';
+	int isupper = 0;
+
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+		if (c == uppercase)
+		{
+			isupper = 1;
+			break;
+		}
+	}
+
+	return (isupper);
 }
